36e2f6bdc9084d9396355c66c828c46f38ffb798
BassimEhab <bassimstudy@gmail.com>
2024-12-13 16:33:34 +0200

    Q2 solved

--- Demo2/Program.cs
+++ Demo2/Program.cs
@@ -312,35 +312,35 @@ namespace Demo2
             #endregion
             #region Assignment
             #region Q1
-            1 - Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4
-            otherwise print no.
-            Console.WriteLine("Please enter a numberber : ");
-            int number = int.Parse(Console.ReadLine());
-            if (number % 3 == 0 && number % 4 == 0)
-            {
-                Console.WriteLine("Yes");
+            //1 - Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4
+            //otherwise print no.
+            //Console.WriteLine("Please enter a numberber : ");
+            //int number = int.Parse(Console.ReadLine());
+            //if (number % 3 == 0 && number % 4 == 0)
+            //{
+            //    Console.WriteLine("Yes");
 
-            }
-            else
-            {
-                Console.WriteLine("No");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No");
 
-            }
+            //}
 
             #endregion
             #region Q2
             //Write a program that allows the user to insert an integer then print negative if it is negative 
             //number otherwise print positive.
-            //Console.Write("Please enter a number:"); 
-            //int Number = int.Parse(Console.ReadLine());
-            //if (Number > 0)
-            //{
-            //    Console.WriteLine("Positive");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Negative");
-            //}
+            Console.Write("Please enter a number:");
+            int Number = int.Parse(Console.ReadLine());
+            if (Number > 0)
+            {
+                Console.WriteLine("Positive");
+            }
+            else
+            {
+                Console.WriteLine("Negative");
+            }
             #endregion
             #region Q3
 
