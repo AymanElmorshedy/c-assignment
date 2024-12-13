382f2b36cab756e60ae152d277d774a20d4ce8a7
BassimEhab <bassimstudy@gmail.com>
2024-12-13 16:32:41 +0200

    Q1 solved

--- Demo2/Program.cs
+++ Demo2/Program.cs
@@ -312,20 +312,20 @@ namespace Demo2
             #endregion
             #region Assignment
             #region Q1
-            //1 - Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 
-            //otherwise print no.
-            //Console.WriteLine("Please enter a numberber : ");
-            //int number=int.Parse(Console.ReadLine());
-            //if (number % 3 == 0 && number % 4 == 0)
-            //{
-            //    Console.WriteLine("Yes");
+            1 - Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4
+            otherwise print no.
+            Console.WriteLine("Please enter a numberber : ");
+            int number = int.Parse(Console.ReadLine());
+            if (number % 3 == 0 && number % 4 == 0)
+            {
+                Console.WriteLine("Yes");
 
-            //}
-            //else
-            //{
-            //    Console.WriteLine("No");
+            }
+            else
+            {
+                Console.WriteLine("No");
 
-            //}
+            }
 
             #endregion
             #region Q2
